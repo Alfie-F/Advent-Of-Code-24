c7e1a4157e68e80c2817afbd5e4e8f05cb98d733
Alfie-F <alfie.fenables99@gmail.com>
2024-12-02 16:38:18 +0000

    adheres to better  naming conventions

--- day-1/part1/part1.js
+++ day-1/part1/part1.js
@@ -5,21 +5,21 @@ const lines = fs
   .trim("")
   .split(/\n|   /);
 
-let A = [];
-let B = [];
-let C = 0;
+let arr1 = [];
+let arr2 = [];
+let sum = 0;
 
 function doTheThing(input) {
   for (let i = 0; i < input.length; i += 2) {
-    A.push(Number(input[i]));
-    B.push(Number(input[i + 1]));
+    arr1.push(Number(input[i]));
+    arr2.push(Number(input[i + 1]));
   }
-  A.sort((a, b) => a - b);
-  B.sort((a, b) => a - b);
-  for (let i = 0; i < A.length; i++) {
-    C += Math.abs(A[i] - B[i]);
+  arr1.sort((a, b) => a - b);
+  arr2.sort((a, b) => a - b);
+  for (let i = 0; i < arr1.length; i++) {
+    sum += Math.abs(arr1[i] - arr2[i]);
   }
+  console.log(sum);
 }
 
 doTheThing(lines);
-console.log(C);
--- day-1/part2/part2.js
+++ day-1/part2/part2.js
@@ -5,30 +5,30 @@ const lines = fs
   .trim("")
   .split(/\n|   /);
 
-let A = [];
-let B = [];
-const C = {};
-let D = 0;
+let arr1 = [];
+let arr2 = [];
+const dict = {};
+let sum = 0;
 
 function doTheThing(input) {
   for (let i = 0; i < input.length; i += 2) {
-    A.push(Number(input[i]));
-    B.push(Number(input[i + 1]));
+    arr1.push(Number(input[i]));
+    arr2.push(Number(input[i + 1]));
   }
-  B.forEach((num) => {
-    if (C[num]) {
-      C[num] += 1;
+  arr2.forEach((num) => {
+    if (dict[num]) {
+      dict[num] += 1;
     } else {
-      C[num] = 1;
+      dict[num] = 1;
     }
   });
-  for (let i = 0; i < A.length; i++) {
-    if (!C[A[i]]) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (!dict[arr1[i]]) {
     } else {
-      D += A[i] * C[A[i]];
+      sum += arr1[i] * dict[arr1[i]];
     }
   }
+  console.log(sum);
 }
 
 doTheThing(lines);
-console.log(D);
