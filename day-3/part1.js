fba961d611a04d321152c734634b78d1b1586fc1
Alfie-F <alfie.fenables99@gmail.com>
2024-12-04 16:33:11 +0000

    completes day 3

--- day-3/part1.js
+++ day-3/part1.js
@@ -4,12 +4,21 @@ const source = fs.readFileSync("./input.txt", "utf-8");
 
 let count;
 let sum = 0;
+let bool = true;
 
 function doTheThingThree(input) {
-  count = input.match(new RegExp(/mul\(\d{1,3},\d{1,3}\)/, "g")) || [];
+  count =
+    input.match(new RegExp(/mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)/, "g")) ||
+    [];
   count.forEach((element) => {
-    let actOn = element.replace(/[a-z()]/g, "").split(",");
-    sum += actOn[0] * actOn[1];
+    if (element === "do()") {
+      bool = true;
+    } else if (element === "don't()") {
+      bool = false;
+    } else if (bool === true) {
+      let actOn = element.replace(/[a-z()]/g, "").split(",");
+      sum += actOn[0] * actOn[1];
+    }
   });
   console.log(sum);
 }
